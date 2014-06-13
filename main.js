00049ff258e9e008f7df65fe02708e2a85d2e048
Ben Kulbertis <ben@kulbertis.org>
2014-06-12 23:23:15 -0600

    Got picky about the JS formatting. Going to bed now...

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     /* Yeah, that's right, brush that weak ass fade. - https://i.imgur.com/OTRjYgU.gif */
     $('header > h1').delay(100).animate({opacity:1}, 400, function(){
         $('header > h2:nth-of-type(1)').delay(100).animate({opacity:1}, 400, function(){
@@ -9,14 +10,13 @@ $(document).ready(function(){
             });
         });
     });
+
     /* AJAXify */
     var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
     $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
         e.preventDefault();
         History.pushState({}, "", this.pathname);
     });
-
     History.Adapter.bind(window, 'statechange', function(){
         var State = History.getState();
         $.get(State.url, function(data){
@@ -27,8 +27,10 @@ $(document).ready(function(){
             //_gaq.push(['_trackPageview', State.url]);
         });
     });
+
     /* Dynamic Resize */
     $(window).resize(function(){
         $('section').height(window.innerHeight-40);
     }).resize();
+
 });
