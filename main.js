f299075471d2418933b03d3d5308609d24b26505
Ben Kulbertis <ben@kulbertis.org>
2014-06-12 20:02:50 -0600

    Ajaxy as hell son.

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+    /* AJAXify */
+    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+        e.preventDefault();
+        History.pushState({}, "", this.pathname);
+    });
+
+    History.Adapter.bind(window, 'statechange', function(){
+        var State = History.getState();
+        $.get(State.url, function(data){
+            document.title = $(data).filter("title").text();
+            $('.post').removeClass('fadein').fadeOut(400, function(){
+                $('.post').html($(data).filter('.post').contents()).fadeIn(400);
+            });
+            //_gaq.push(['_trackPageview', State.url]);
+        });
+    });
+    /* Dynamic Resize */
     $(window).resize(function(){
         $('section').height(window.innerHeight-40);
     }).resize();
